dfea88d5ed10af32a8dc15ec72ebadeba31a7b9b
Dan <danyoungmusic93@gmail.com>
2020-07-10 23:54:43 -0400

    messing with regex

--- server/tests/test_helpers.js
+++ server/tests/test_helpers.js
@@ -9,7 +9,7 @@ describe('tests', function () {
     })
     it("Should return a normalized string", () => {
       assert.equal(normalizeSongName("Hello.mp3"), "Hello")
-      assert.equal(normalizeSongName("04 New Monastery.mp3", "New Monastery"))
+      assert.equal(normalizeSongName("04 New Monastery.mp3"), "New Monastery")
       assert.equal(normalizeSongName("06 T.C..mp3", "New Monastery"), "T.C.")
       assert.equal(normalizeSongName("07 15_8.mp3", "New Monastery"), "15/8")
     })
